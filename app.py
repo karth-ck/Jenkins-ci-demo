d31091c3509a73d1b26fae21fa54cbf1c1e3b0ca
karthik <karhikck215@gmail.com>
2025-09-26 13:04:04 +0530

    add updated app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 def greet(name):
     return f"Hello, {name}!"
+
+# Call function directly
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    message = greet(user_name)
+    print(message)
